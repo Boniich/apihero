de16ec1df59db50606bf0f00a399d30e89da0970
boniich <ezequieldbo25@gmail.com>
2023-06-05 19:37:17 -0300

    feat: end character crud

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Character;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -58,32 +59,57 @@ class CharacterController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Character $character)
+    public function show($id)
     {
-        //
-    }
+        $character = Character::find($id);
+        $character = $character->makeHidden(['created_at', 'updated_at']);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Character $character)
-    {
-        //
+        if (is_null($character)) {
+            return response()->json(['error:' => 'character not found'], 404);
+        }
+
+        return $character;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Character $character)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $character = Character::find($id);
+
+            if (is_null($character)) {
+                return response()->json(['error:' => 'character not found'], 404);
+            }
+
+            $character->name = $request->name;
+            $character->age = $request->age;
+            $character->wight = $request->wight;
+            $character->history = $request->history;
+            $character->image = $request->image;
+
+            $character->update();
+
+            return response()->json($character);
+        } catch (\Exception $ex) {
+            return response()->json(['error' => 'Bad request'], 400);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Character $character)
+    public function destroy($id)
     {
-        //
+        $character = Character::find($id);
+
+        if (is_null($character)) {
+            return response()->json(['error:' => 'character not found'], 404);
+        }
+
+        $character->delete();
+
+        return response()->noContent();
     }
 }
