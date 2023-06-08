56192e7630f0e2b74df0b6b32184779b81ae4f7c
boniich <ezequieldbo25@gmail.com>
2023-06-08 19:32:18 -0300

    feat: add functions to delete and update images

--- app/Helpers/ImagesFunctions.php
+++ app/Helpers/ImagesFunctions.php
@@ -1,13 +1,38 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
-if (!function_exists('loadImage')) {
+if (!function_exists('upLoadImage')) {
 
     function upLoadImage($imageInRequest)
     {
         $imageName = time() . '.' . $imageInRequest->getClientOriginalExtension();
         Storage::disk('public')->put($imageName, file_get_contents($imageInRequest));
+
+        return $imageName;
+    }
+}
+
+if (!function_exists('deleteLoadedImage')) {
+
+    function deleteLoadedImage($imageLoaded)
+    {
+
+        $isThereAnImage = Storage::disk('public')->exists($imageLoaded);
+
+        if ($isThereAnImage) {
+            Storage::disk('public')->delete($imageLoaded);
+        }
+    }
+}
+
+if (!function_exists('updateLoadedImage')) {
+    function updateLoadedImage($imageLoaded, $imageToLoad)
+    {
+
+        deleteLoadedImage($imageLoaded);
+        $imageName = upLoadImage($imageToLoad);
 
         return $imageName;
     }
