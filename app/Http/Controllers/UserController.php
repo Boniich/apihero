b750a4319c3f4615baa5aed49b95fb59c04ce5c4
boniich <ezequieldbo25@gmail.com>
2023-06-11 16:50:12 -0300

    feat: add routes and methods to handle the login and register

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        try {
+
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed'
+            ]);
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+
+            $user->save();
+
+            return response()->json(successResponse($user, "User created successfully"));
+        } catch (\Throwable $th) {
+            return response()->json(errorResponse("Bad Request"), 400);
+        }
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('token')->plainTextToken;
+
+            return response()->json(["success" => true, "token" => $token, "message" => "Token created successfully"]);
+        } else {
+            return response()->json(errorResponse("Invalid Credentials"), 401);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CharacterController;
 use App\Http\Controllers\FilmController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
 
 Route::apiResource('characters', CharacterController::class);
 Route::get('searchCharacter/{name?}/{age?}', [CharacterController::class, 'search']);
