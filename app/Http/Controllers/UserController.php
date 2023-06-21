15872607e1b58312d60b475b0868f76782da8261
boniich <ezequieldbo25@gmail.com>
2023-06-20 21:10:12 -0300

    doc: add doctumentacion about auth user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,39 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
+
+    /**
+     * Register a new user.
+     * @OA\Post(
+     *      path="/api/register",
+     *      summary="Register a new user",
+     *      tags={"Auth"},
+     * 
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          required={"name","email","password","password_confirmation"},
+     *          @OA\Property(property="name", type="string", format="string"),
+     *          @OA\Property(property="email", type="string", format="string"),
+     *          @OA\Property(property="password", type="string", format="string" ),
+     *          @OA\Property(property="password_confirmation", type="string", format="string" ),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="User register successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request"  
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="An error has occurred"
+     *      )
+     * )
+     */
+
     public function register(Request $request)
     {
 
@@ -28,11 +61,41 @@ class UserController extends Controller
 
             $user->save();
 
-            return response()->json(successResponse($user, "User created successfully"));
+            return response()->json(successResponse($user, "User register successfully"));
         } catch (\Throwable $th) {
             return response()->json(errorResponse("Bad Request"), 400);
         }
     }
+
+    /**
+     * Login User.
+     * @OA\Post(
+     *      path="/api/login",
+     *      summary="Login user",
+     *      tags={"Auth"},
+     * 
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *          required={"email","password"},
+     *          @OA\Property(property="email", type="string", format="string"),
+     *          @OA\Property(property="password", type="string", format="string" ),
+     *                    ),
+     *              ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="User login successfully"  
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="bad request"  
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="An error has occurred"
+     *      )
+     * )
+     */
 
     public function login(Request $request)
     {
